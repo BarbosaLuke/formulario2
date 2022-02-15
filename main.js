224b73d141e7fe69bfe5cdc924f918a0d3680bb2
BarbosaLuke <91038070+BarbosaLuke@users.noreply.github.com>
2022-02-15 14:16:26 -0300

    boxcadastro.css
    criando o box cadastro e fazendo os botões abrirem e fecharem as boxs

---
+++ main.js
@@ -0,0 +1,39 @@
+'use strict'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ABRIR E FECHAR A BOXEDIT
+const openBoxEdit = () => document.getElementById('box-edit')
+    .classList.add('active')
+const closeBoxEdit = () => document.getElementById('box-edit')
+    .classList.remove('active')
+
+
+//ABRIR E FECHAR A BOXSHOW   
+const openBoxShow = () => document.getElementById('box-show')
+    .classList.add('active')
+const closeBoxShow = () => document.getElementById('box-show')
+    .classList.remove('active')
+
+
+//ABRIR E FECHAR A BOXCADASTRO   
+const openBoxCadastro = () => document.getElementById('box-cadastro')
+    .classList.add('active')
+const closeBoxCadastro = () => document.getElementById('box-cadastro')
+    .classList.remove('active')
+
