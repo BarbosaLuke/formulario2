8c2c9afe21d5d396326f2e890638d2fd27946149
BarbosaLuke <91038070+BarbosaLuke@users.noreply.github.com>
2022-02-16 20:19:50 -0300

    adicionando o formulario criado na tela

--- main.js
+++ main.js
@@ -33,9 +33,61 @@ const createForm =  (form) =>{
     dbForm.push(form)
     setLocalStorage(dbForm)
 }
+const isValidFields = () =>{
+   return document.getElementById('cadastro-form').reportValidity()
+}
+//Interação com layout
 
+const clearFields = () =>{
+    const fields = document.querySelectorAll('.box-field-cadastro')
+    fields.forEach(field => field.value = "")
+}
 
+const saveForm = () => {
+    if (isValidFields()){
+        
+        const form = {
+            nome: document.getElementById('nome').value,
+            CPF: document.getElementById('CPF').value,
+            nomeDaMae: document.getElementById('nomeDaMae').value,
+            nomeDoPai: document.getElementById('nomeDoPai').value,
+            corPreferida: document.getElementById('corPreferida').value,
+            dataNascimento: document.getElementById('dataNascimento').value,
+            email: document.getElementById('email').value
+        }
+        createForm(form)
+        clearFields()
+        closeBoxCadastro()
+    }
 
+}
+
+const CreateRow = (form,index) =>{
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td class="id">${index+1}</td>
+        <td class="nome">${form.nome}</td>
+        <td class="rowbutton">
+            <button onclick="openBoxShow()" id="show" class="button azul" type="button">
+                Show
+            </button>
+            <button onclick="openBoxEdit()" id="edit" class="button amarelo" type="button">
+                Editar
+            </button>
+            <button onclick="" class="button vermelho" type="button">
+                Excluir
+            </button>
+        </td>
+    `
+    document.querySelector('#tableForm>tbody').appendChild(newRow)
+}
+
+const updateTable = () =>{
+    const dbForm = readForm()
+    dbForm.forEach(CreateRow)
+}
+
+updateTable()
 
 //ABRIR E FECHAR A BOXEDIT
 const openBoxEdit = () => document.getElementById('box-edit')
@@ -54,5 +106,7 @@ const closeBoxShow = () => document.getElementById('box-show')
 //ABRIR E FECHAR A BOXCADASTRO   
 const openBoxCadastro = () => document.getElementById('box-cadastro')
     .classList.add('active')
-const closeBoxCadastro = () => document.getElementById('box-cadastro')
+const closeBoxCadastro = () => {document.getElementById('box-cadastro')
     .classList.remove('active')
+    clearFields()
+}
